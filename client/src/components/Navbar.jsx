f3fee2546fd7b7b6e474c749a022eb0219225f0c
deshinta <deshintaaulia065@gmail.com>
2024-06-23 20:02:51 +0700

    style: styling home page and navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 export default function Navbar() {
 
     return <>
-    <div className="py-4 pl-9 text-[#f15858] bg-white">
+    <div className="py-3 pl-10 text-[#f15858] bg-white">
         <h1 className="font-bold text-lg">Timesheet</h1>
         <h1 className="font-bold text-sm">Management</h1>
     </div>
-    <div className="bg-white mt-2">
-            <h1 className="font-bold text-2xl py-6 pl-9">HH Timesheet</h1>
-            <div className="flex gap-5 text-lg pl-12">
+    <div className="bg-white mt-1">
+            <h1 className="font-bold text-xl py-4 pl-7">HH Timesheet</h1>
+            <div className="flex gap-5 text-base pl-12">
                 <div className="hover:text-[#2775EC] focus:text-[#2775EC] cursor-pointer active:ring ring-[#F0F6FF]">
                     <h1 className="mb-3">Daftar Kegiatan</h1>
                     <hr />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,45 +1,87 @@
 export default function Home(){
     return (
         <>
-        <div className="m-7 bg-white rounded-lg">
+        <div className="m-5 bg-white rounded-lg">
             {/* <h1 className="font-bold text-center text-2xl">Table</h1> */}
-            <div className="flex p-7">
+            <div className="flex p-5">
                 <div className="mr-16">
-                    <h1>Nama Karyawan</h1>
-                    <h3 className="text-xl">User 1</h3>
+                    <h1 className="text-sm">Nama Karyawan</h1>
+                    <h3 className="text-base">User 1</h3>
                 </div>
                 <div>
-                    <h1>Rate</h1>
-                    <h3 className="text-xl">Rp 12.000/jam</h3>
+                    <h1 className="text-sm">Rate</h1>
+                    <h3 className="text-base">Rp 12.000/jam</h3>
                 </div>
             </div>
             <hr />
-            <div className="flex p-7 gap-5">
-                <h1>Daftar Kegiatan</h1>
-                <button className="py-1 px-2 rounded bg-[#F7F8FB] text-[#2775EC]">Tambah Kegiatan</button>
+            <div className="flex justify-between p-6 text-base">
+                <div className="flex gap-5 font-bold">
+                    <h1>Daftar Kegiatan</h1>
+                    <button className="py-1 px-2 rounded bg-[#F7F8FB] text-[#2775EC]">Tambah Kegiatan</button>
+                </div>
+                <div>
+                    <form action="">
+                        <input type="text" className="rounded py-1 px-2 border focus:outline-none focus:ring-1 ring-[#F7F8FB] mr-3" />
+                        <button type="submit" className="py-1 px-2 rounded bg-[#2775EC] text-white">cari</button>
+                    </form>
+                </div>
             </div>
-            <div className="p-6">
-                <table className="table-auto text-center min-w-full divide-y divide-[#F7F8FB] rounded-lg ring ring-[#F7F8FB] ring-offset-0">
-                    <thead className="">
-                        <tr>
-                            <th>Song</th>
-                            <th>Artist</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y divide-[#F7F8FB]">
-                        <tr>
-                            <td>The Sliding Mr. Bones</td>
-                            <td>Malcolm Lockyer</td>
-                            <td>1961</td>
-                        </tr>
-                        <tr>
-                            <td>The Sliding Mr. Bones</td>
-                            <td>Malcolm Lockyer</td>
-                            <td>1961</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="px-5 pb-5">
+                <div className="table-auto min-w-full divide-y divide-[#F7F8FB] rounded-lg ring ring-[#F7F8FB] ring-offset-0 text-base">
+                    <table className="min-w-full text-center">
+                        <thead className="font-bold">
+                            <tr>
+                                <th>Judul Kegiatan</th>
+                                <th>Nama Proyek</th>
+                                <th>Tanggal Mulai</th>
+                                <th>Tanggal Berakhir</th>
+                                <th>Waktu Mulai</th>
+                                <th>Waktu Berakhir</th>
+                                <th>Durasi</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-[#F7F8FB]">
+                            <tr>
+                                <td>Pembuatan Website</td>
+                                <td>UI Desain</td>
+                                <td>2 Okt 2024</td>
+                                <td>3 Okt 2024</td>
+                                <td>09.00</td>
+                                <td>15.00</td>
+                                <td>06.00</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Pembuatan Server</td>
+                                <td>UI Desain</td>
+                                <td>3 Okt 2024</td>
+                                <td>4 Okt 2024</td>
+                                <td>09.00</td>
+                                <td>15.00</td>
+                                <td>06.00</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="bg-[#F7F8FB] text-[#2775EC] p-3">
+                        <div className="flex justify-between">
+                            <h1>Total Durasi</h1>
+                            <h1>8 jam 50 menit</h1>
+                        </div>
+                        <div className="flex justify-between font-bold">
+                            <span>Total Pendapatan</span>
+                            <span>Rp 153.000</span>
+                        </div>
+                        <h1></h1>
+                    </div>
+                </div>
             </div>
         </div>
         </>
